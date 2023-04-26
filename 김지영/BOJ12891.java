6877d0d1770ec03039a0e2af31b79bc077d3400d
Kim-Ji-Yeong <yeong6415@daum.net>
2023-04-26 19:12:20 +0900

    230426_BOJ12891 DNA 비밀번호

---
+++ 김지영/BOJ12891.java
@@ -0,0 +1,62 @@
+package com.example.cordingtestpractice.Jiyeong;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ12891 {
+    static int checkArr[];
+    static int myArr[];
+    static int checkSecret;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(st.nextToken());  // 문자열 크기
+        int P = Integer.parseInt(st.nextToken());  // 부분 문자열 크기
+        int Result = 0;
+        char A[] = new char[S];
+        checkArr = new int[4];  // 비밀번호 체크 배열
+        myArr = new int[4];  // 현재 배열 상태
+        checkSecret = 0;
+
+        A = br.readLine().toCharArray();  // 전체 배열 받기
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());  // 숫자 4개
+            if (checkArr[i] == 0)
+                checkSecret++;  // 4가 되면 충족
+        }
+
+        for (int i = 0; i < P; i++) {
+            Add(A[i]);
+        }
+    }
+        private static void Add(char c){
+            switch (c) {
+                case 'A':
+                    myArr[0]++;
+                    if (myArr[0] == checkArr[0])
+                        checkSecret++;
+                    break;
+                case 'C':
+                    myArr[1]++;
+                    if (myArr[1] == checkArr[1])
+                        checkSecret++;
+                    break;
+                case 'G':
+                    myArr[2]++;
+                    if (myArr[2] == checkArr[2])
+                        checkSecret++;
+                    break;
+                case 'T':
+                    myArr[3]++;
+                    if (myArr[3] == checkArr[3])
+                        checkSecret++;
+                    break;
+            }
+        }
+    }
+
